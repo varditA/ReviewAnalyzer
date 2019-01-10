b24656b20f9c59d05626d3bc2b6f24f3fa248f1b
Vardit Arkash <rosea24682@gmail.com>
2019-01-10 16:27:10 +0200

    updated README with files list. updated Manager and files hirarchy

--- crawler.py
+++ crawler.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import time
+
+# todo upsate the appList with the apps' names
+appsList = list()
 
 
 def getReviews(projectsDict):
@@ -16,7 +20,8 @@ def getReviews(projectsDict):
         href = link.a.get("href")
         projects_urls.add("https://play.google.com" + href)
     for project_url in projects_urls:
-        driver.implicitly_wait(30)
+        # driver.implicitly_wait(30)
+        time.sleep(2)
         driver.get(project_url + "&showAllReviews=true")
         new_soup = BeautifulSoup(driver.page_source, 'html.parser')
         span = new_soup.findAll('span', jsname="bN97Pc")
@@ -24,6 +29,9 @@ def getReviews(projectsDict):
         for s in span:
             reviews.append(s.text)
         projectsDict[project_url] = reviews
+        # print(reviews)
+
+    driver.close()
 
 
 if __name__ == "__main__":
