0a4b0f830f1ab9301aec06c06f4ae74e85523e1a
Vardit Arkash <rosea24682@gmail.com>
2019-01-10 16:00:21 +0200

    added crawler

---
+++ crawler.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+
+
+def getReviews(projectsDict):
+    projects_urls = set()
+    url = 'https://play.google.com/store/apps/collection/recommended_for_you?clp=ogoKCAEqAggBUgIIAQ%3D%3D:S:ANO1ljJG6Aw&gsr=Cg2iCgoIASoCCAFSAggB:S:ANO1ljLKNqE'
+    # setting up connection, get the info and closing it
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(30)
+    driver.get(url)
+
+    html_soup = BeautifulSoup(driver.page_source, 'lxml')
+    # html_soup.findAll('div', {"class": "card no-rationale square-cover apps small"})
+    for link in html_soup.findAll('div', {"class": "card no-rationale square-cover apps small"}):
+        href = link.a.get("href")
+        projects_urls.add("https://play.google.com" + href)
+    for project_url in projects_urls:
+        driver.implicitly_wait(30)
+        driver.get(project_url + "&showAllReviews=true")
+        new_soup = BeautifulSoup(driver.page_source, 'html.parser')
+        span = new_soup.findAll('span', jsname="bN97Pc")
+        reviews = []
+        for s in span:
+            reviews.append(s.text)
+        projectsDict[project_url] = reviews
+
+
+if __name__ == "__main__":
+    projects = {}
+    getReviews(projects)
